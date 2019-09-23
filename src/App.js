e34f2b61494af0b872e3a93697da9e48bc4ed83f
sarah <sarahkharraz@gmail.com>
2019-09-23 16:28:16 +0100

    Start filter by prices input

--- src/App.js
+++ src/App.js
@@ -19,19 +19,40 @@ function App() {
       <section className="filters">
         <h1>Burger Place</h1>
         <h2>Filters</h2>
-        <form>Inputs go here</form>
+
+        <form>
+          <fieldset>
+            <legend>Price</legend>
+            <label for="minPrice">Min Price</label>
+            <input
+              type="range"
+              id="minPrice"
+              name="minPrice"
+              min="0"
+              max="10"
+            />
+            <label for="maxPrice">Max Price</label>
+            <input
+              type="range"
+              id="maxPrice"
+              name="maxPrice"
+              min="0"
+              max="10"
+            />
+          </fieldset>
+        </form>
       </section>
       <section className="dishes">
         <h2>Dishes</h2>
 
         <ul className="grid">
-            {data.map((dish, i) => (
-              <li className="card" key={i}>
-                <h3>{dish.name}</h3>
-                <p>{dish.description}</p>
-                <p>${dish.price}</p>
-              </li>
-            ))}
+          {data.map((dish, i) => (
+            <li className="card" key={i}>
+              <h3>{dish.name}</h3>
+              <p>{dish.description}</p>
+              <p>£{dish.price}</p>
+            </li>
+          ))}
         </ul>
       </section>
     </main>
