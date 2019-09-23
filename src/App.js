834aed268a2f3ea6a4ea78c82a764500343df218
sarah <sarahkharraz@gmail.com>
2019-09-23 17:31:48 +0100

    create PriceFilter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import dishes from "./data";
+// import dishes from "./data";
 import data from "./data";
 
 // const categories = [
@@ -13,38 +13,48 @@ import data from "./data";
 //   "extra",
 // ];
 
+const PriceFilter = ({ state, setState }) => {
+  return (
+    <fieldset>
+      <legend>Price</legend>
+      <label htmlFor="minPrice">Min Price</label>
+      <input
+        type="range"
+        id="minPrice"
+        name="minPrice"
+        min="0"
+        max="9"
+        value={state[0]}
+        onChange={e => setState([e.target.value, state[1]])}
+      />
+      <label htmlFor="maxPrice">Max Price</label>
+      <input
+        type="range"
+        id="maxPrice"
+        name="maxPrice"
+        min="0"
+        max="9"
+        value={state[1]}
+        onChange={e => setState([state[0], e.target.value])}
+      />
+    </fieldset>
+  );
+};
+
 function App() {
+  const [state, setState] = React.useState([0, 9]);
+  // const handleChange = event => setState(0, 9);
   return (
     <main>
       <section className="filters">
         <h1>Burger Place</h1>
         <h2>Filters</h2>
-
         <form>
-          <fieldset>
-            <legend>Price</legend>
-            <label for="minPrice">Min Price</label>
-            <input
-              type="range"
-              id="minPrice"
-              name="minPrice"
-              min="0"
-              max="10"
-            />
-            <label for="maxPrice">Max Price</label>
-            <input
-              type="range"
-              id="maxPrice"
-              name="maxPrice"
-              min="0"
-              max="10"
-            />
-          </fieldset>
+          <PriceFilter state={state}></PriceFilter>
         </form>
       </section>
       <section className="dishes">
         <h2>Dishes</h2>
-
         <ul className="grid">
           {data.map((dish, i) => (
             <li className="card" key={i}>
